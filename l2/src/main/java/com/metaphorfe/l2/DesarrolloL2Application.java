e60bb51cd3b633b82923b66cdecc1e5978095294
JuanCaballero4510 <jcaballerog4510@gmail.com>
2022-07-09 20:18:14 -0500

    creacion de proyecto

---
+++ l2/src/main/java/com/metaphorfe/l2/DesarrolloL2Application.java
@@ -0,0 +1,13 @@
+package com.metaphorfe.l2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesarrolloL2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesarrolloL2Application.class, args);
+	}
+
+}
---
+++ l2/src/test/java/com/metaphorfe/l2/DesarrolloL2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.metaphorfe.l2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesarrolloL2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
